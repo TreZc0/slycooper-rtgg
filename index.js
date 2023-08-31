88b569388897b5c4e5d719e7c04684d8fe0894e3
TreZc0 <trezc0@gmx.de>
2023-08-31 13:18:00 +0200

    feat: cleanup, help command, remove persistence

--- index.js
+++ index.js
@@ -1,43 +1,34 @@
 'use strict';
 // Modules
-const fs = require('fs');
 const { URL, URLSearchParams } = require('url');
 const { v4: uuidv4 } = require('uuid');
 const axios = require('axios');
 const WebSocket = require('ws');
-const jsonfile = require('jsonfile');
 
 // Config
 const config = require('./config.json');
 
 // Runtime vars
-var currentlyCommitting = false;
-var invalidRaceStates = ["finished", "in_progress", "cancelled"];
-var trackedRaceRooms = {};
+const debugLog = config["verbose-logging"];
+const invalidRaceStates = ["finished", "in_progress", "cancelled"];
 
 // State Init
-const stateFile = './state.json';
-const stateDefault = {
+const state = {
     accessToken: "",
-    expiry: 0
+    expiry: 0,
+    trackedRaceRooms: {}
 };
 
-var state;
-
-if (!fs.existsSync(stateFile)) {
-    jsonfile.writeFileSync(stateFile, stateDefault);
-    state = stateDefault;
-} else {
-    state = jsonfile.readFileSync(stateFile);
-
-    if (!("accessToken" in state)) {
-        state = stateDefault;
-        commitState();
-    }       
-}
-
-
+/**
+ * REST request against rtgg category API
+ * loop over all races, discard ones that are not in preparation phase
+ * grab websocket url for new races through REST request
+ * init new websocket connection by creating new rtggRace class objects
+ */
 async function checkForNewRaces() {
+    if (debugLog)
+        console.log(`${new Date().toISOString()} - checking for new races`);
+
     const categoryURL = new URL(`/${config["rtgg-game-tag"]}/data`, config["rtgg-host"])
 
     const res = await axios.get(categoryURL);
@@ -47,7 +38,7 @@ async function checkForNewRaces() {
     for (let i = 0; i < races.length; i++) {
         let currentRace = races[i];
 
-        if (currentRace.name in trackedRaceRooms)
+        if (currentRace.name in state.trackedRaceRooms)
             continue;
 
         if (invalidRaceStates.includes(currentRace.status.value.toLowerCase()))
@@ -64,14 +55,13 @@ async function checkForNewRaces() {
 
         const raceWSURL = new URL(`${raceInfo.data.websocket_bot_url}?token=${state.accessToken}`, config["rtgg-websocket"]);
 
+        if (debugLog)
+            console.log(`${new Date().toISOString()} - joining race room ${currentRace.name}`);
+
         const newRace = new rtggRace(currentRace.name, raceWSURL);
 
-        trackedRaceRooms[currentRace.name] = newRace;
+        state.trackedRaceRooms[currentRace.name] = newRace;
     }
-
-    setTimeout(() => {
-        checkForNewRaces();
-    },10000);
 }
 
 // Race Room Class
@@ -83,7 +73,9 @@ class rtggRace {
         let raceRoom = this;
 
         this.ws.onopen = function() {
-            console.log('WS connection successful: ' + raceRoom.name);
+            if (debugLog)
+                console.log(`${new Date().toISOString()} - sent welcome message to ${raceRoom.name}`);
+
             const introMsg = 'Welcome to Sly Cooper and the Thievious Raccoonus! I am Interpol, your friendly randomizer bot. Type !seed to have me roll a randomizer seed for you.';
             raceRoom.sendMessage(introMsg);
         }
@@ -94,7 +86,7 @@ class rtggRace {
 
                 if (invalidRaceStates.includes(data.race.status.value)) {
                     this.close();
-                    delete trackedRaceRooms[raceRoom.name];
+                    delete state.trackedRaceRooms[raceRoom.name];
                 }
                 return;
             }
@@ -114,6 +106,10 @@ class rtggRace {
                 if (command == "!seed") {
                     let parameters = data.message.message_plain.toLowerCase().trim().split(" ");
                     if (raceRoom.seedURL == null || (raceRoom.seedURL != null && parameters.length > 1 && parameters[1].trim().toLowerCase() == "--force")) {
+
+                        if (debugLog)
+                            console.log(`${new Date().toISOString()} - seed requested in ${raceRoom.name}`);
+
                         var randomSeed = randomizeSeed();
 
                         var seedURL = `${config["randomizer-web-host"]}/?seed=${randomSeed}`;
@@ -127,11 +123,19 @@ class rtggRace {
                             }
                         });
 
+                        if (debugLog)
+                            console.log(`${new Date().toISOString()} - seed generated for ${raceRoom.name}: ${seedURL}`);
+
                         raceRoom.seedURL = seedURL;
                         raceRoom.ws.send(raceRoomInfo);  
                     } else {
-                        raceRoom.sendMessage("Sorry, I already created a seed for this race. Please use !seed --force to roll a new one anyway");
+                        if (debugLog)
+                            console.log(`${new Date().toISOString()} - seed request rejected in ${raceRoom.name}`);
+
+                        raceRoom.sendMessage("Sorry, I already created a seed for this race. Please use !seed --force to roll a new one anyway and replace the existing seed.");
                     }
+                } else if (command == "!help") {
+                    raceRoom.sendMessage("Welcome to Sly Cooper and the Thievious Raccoonus! I am Interpol, your friendly randomizer bot. Type !seed to have me roll a randomizer seed for you.");
                 }
                 return;
             }
@@ -143,19 +147,22 @@ class rtggRace {
     }
 
     sendMessage(msg) {
-        console.log(msg);
         this.ws.send(JSON.stringify({
             'action': 'message',
             'data': {
                 'guid': uuidv4(),
-                'message': msg
-                
+                'message': msg 
             }
         }));
     }
 }
 
 // Access Token Handling
+/**
+ * request rtgg access token
+ * persists in state var
+ * recursive based on expiry time
+ */
 async function getAccessToken() {
     const params = new URLSearchParams({
         client_id: config["bot-client-id"],
@@ -179,32 +186,26 @@ async function getAccessToken() {
     state.accessToken = res.data.access_token;
     state.expiry = res.data.expires_in;
 
-    setTimeout(getAccessToken, (state.expiry * 1000) - 60000);
-    commitState();
+    if (debugLog)
+        console.log(`${new Date().toISOString()} - acquired new racetime.gg access token`);
 
+    setTimeout(getAccessToken, (state.expiry * 1000) - 60000);
 }
 
 // Helper Functions
+/**
+ * @returns random number between 10 and 13
+ */
 function randomizeSeed() {
     var n = Math.random() * (13 - 10) + 10; //Random number between 10 and 13 for length
     return Math.floor(Math.pow(10, n - 1) + Math.random() * (Math.pow(10, n) - Math.pow(10, n - 1) - 1));
 }
 
-function commitState() {
-    if (!currentlyCommitting) {
-
-        currentlyCommitting = true;
-
-        jsonfile.writeFile(stateFile, state, {
-                spaces: 1
-            })
-            .then(() => {
-                currentlyCommitting = false;
-            })
-            .catch(error => console.error(error));
-    }
-}
-
+/**
+ * recursive function
+ * Check for available access token, then trigger race check routine (checkForNewRaces)
+ * runs every 10 seconds 
+ */
 async function loop() {
 
     if (state.accessToken != undefined && state.accessToken.length > 0) {
@@ -220,12 +221,16 @@ async function loop() {
     setTimeout(loop, 10000);  
 }
 
-//init
+/**
+ * Initialization function
+ * Get rtgg access token, start loop
+ */
 async function init() {
     await getAccessToken();
-    console.log("Bot up!");
+    console.log("Bot up and access token received!");
 
     await loop();
 }
 
+//Init with access token and start loop
 init();
